9829defc39fb98ced96aa5f98dd077837bbc2939
Logan Saso <logansaso@gmail.com>
2019-04-08 16:43:07 -0700

    Do it all in one thread

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
 #![allow(clippy::inconsistent_digit_grouping)]
 
-use hex_slice::AsHex;
+use hashbrown::HashSet;
 use md5::compute;
-use rayon::prelude::*;
-use std::fs::OpenOptions;
-use std::io::{BufWriter, Write};
 
-use std::sync::{Arc, Mutex};
 fn main() {
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open("./collisions.txt")
-        .unwrap();
-    let file = BufWriter::new(file);
-    let file = Arc::new(Mutex::new(file));
-
-    (1_000_000_0000u128..1_999_999_9999u128)
-        .into_par_iter()
-        .for_each(|i| {
-            let bytes: [u8; 16] = compute(i.to_be_bytes()).into();
-            file.lock()
-                .unwrap()
-                .write_fmt(format_args!("{:x}\n", bytes.as_hex()))
-                .unwrap();
-        });
+    let mut set = HashSet::with_capacity(1_999_999_9999 - 1_000_000_0000);
+    (1_000_000_0000u128..1_999_999_9999u128).for_each(move |i| {
+        let bytes: [u8; 16] = compute(i.to_be_bytes()).into();
+        if !set.insert(bytes) {
+            eprintln!("Collision Detected!");
+        }
+    });
 }
