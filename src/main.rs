5c19ef6ca46d407fc0ea8a706c86d3ff381f2e90
Logan Saso <logansaso@gmail.com>
2019-04-08 16:18:50 -0700

    Compute the data in-memory first

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,23 @@ use rayon::prelude::*;
 use std::fs::OpenOptions;
 use std::io::{BufWriter, Write};
 
-use std::sync::{Arc, Mutex};
 fn main() {
     let file = OpenOptions::new()
         .create(true)
         .write(true)
         .open("./collisions.txt")
         .unwrap();
-    let file = BufWriter::new(file);
-    let file = Arc::new(Mutex::new(file));
+    let mut file = BufWriter::new(file);
 
-    (1_000_000_0000u128..1_999_999_9999u128)
+    let par_iter = (1_000_000_0000u128..1_999_999_9999u128)
         .into_par_iter()
-        .for_each(|i| {
-            let bytes: [u8; 16] = compute(i.to_be_bytes()).into();
-            file.lock()
-                .unwrap()
-                .write_fmt(format_args!("{:x}\n", bytes.as_hex()))
-                .unwrap();
+        .map(|i| {
+            let computed: [u8; 16] = compute(i.to_be_bytes()).into();
+            computed
         });
+    let res = par_iter.collect::<Vec<_>>();
+    res.into_iter().for_each(|hex: [u8; 16]| {
+        file.write_fmt(format_args!("{:x}\n", hex.as_hex()))
+            .unwrap()
+    });
 }
