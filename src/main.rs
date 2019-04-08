b71a32617722f6bbd5193f23d3c407767b7d5bd2
Logan Saso <logansaso@gmail.com>
2019-04-08 16:57:35 -0700

    Wrute results to a file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #![allow(clippy::inconsistent_digit_grouping)]
 
-use hashbrown::HashSet;
+use hex_slice::AsHex;
 use md5::compute;
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Write};
 
 fn main() {
-    let mut set = HashSet::with_capacity(1_999_999_9999 - 1_000_000_0000);
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open("./collisions.txt")
+        .unwrap();
+    let mut file = BufWriter::new(file);
+
     (1_000_000_0000u128..1_999_999_9999u128).for_each(move |i| {
         let bytes: [u8; 16] = compute(i.to_be_bytes()).into();
-        if !set.insert(bytes) {
-            eprintln!("Collision Detected!");
-        }
+        file.write_fmt(format_args!("{:x}\n", bytes.as_hex())).unwrap();
     });
 }
